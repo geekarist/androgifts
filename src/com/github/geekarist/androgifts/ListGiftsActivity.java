6f600e443be13749d5f267fda927c4c265a05001
Christophe Pelé <cpele@xebia.fr>
2015-12-08 07:38:29 +0100

    Create application

---
+++ src/com/github/geekarist/androgifts/ListGiftsActivity.java
@@ -0,0 +1,15 @@
+package com.github.geekarist.androgifts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListGiftsActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
